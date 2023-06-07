aacde241bc77f4a3f525bdd536aea3161b908291
根路銘啓 <neromehiro@neromekeinoMacBook-Pro.local>
2023-06-07 17:10:55 +0900

    “適切なメモ”

---
+++ index.js
@@ -0,0 +1,15 @@
+const form =  document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    add();
+});
+
+function add(){
+    const li = document.createElement("li");
+    li.innerText=input.value;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    input.value = "";
+}
